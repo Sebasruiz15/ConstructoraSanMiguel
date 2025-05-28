c26843b61cb2302e69effc09d4d0a883d67b37bb
Sebasruiz <sebasruizloaiza@gmail.com>
2025-05-27 20:49:35 -0500

    Creacion de php

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require '../includes/funciones.php';
+ incluirTemplate('header');
+ 
+?>
+
+<main class="contenedor seccion">
+    <h1> Administrador de San Miguel </h1>
+
+    <a href="/admin/propiedades/crear.php" class="boton boton-cafeClaro">Nuevo Proyecto</a>
+</main>
+
+<?php
+ incluirTemplate('footer');
+?>
--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -7,6 +7,28 @@ require '../../includes/funciones.php';
 
 <main class="contenedor seccion">
     <h1>Crear </h1>
+
+    <a href="/admin" class="boton boton-cafeClaro">Volver</a>
+
+    <form action="" class="formulario">
+
+    <fieldset>
+        <legend>Información General</legend>
+
+        <label for="titulo">Titulo:</label>
+        <input type="text" id="titulo"  placeholder="Nombre Proyecto">
+
+         <label for="imegen">Imagen:</label>
+        <input type="file" id="imegen" accept="image/jpeg, image/png" >
+
+        <label for="descripcion">Descripción: </label>
+        <textarea name="" id="descripcion"></textarea>
+    </fieldset>
+
+
+    </form>
+
+
 </main>
 
 <?php
